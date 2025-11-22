0bd024932f0981fe597101b4c8a389c8be192f34
kawacukennedy <kawacukent@gmail.com>
2025-11-22 13:14:29 +0200

    Add Footer component with copyright

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-800 text-white p-4 mt-8">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2025 Autonomous Process Auditor. Powered by IBM watsonx.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
